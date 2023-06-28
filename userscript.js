5b625870a4c72d2d6b2d7b9af4169babaec4756c
Will B Chang <14329786+willbchang@users.noreply.github.com>
2023-06-28 11:00:04 +0800

    fix: 已停止访问该网页

--- userscript.js
+++ userscript.js
@@ -1,17 +1,20 @@
 // ==UserScript==
-// @name         QQ Open Link Directly
+// @name         QQ Open Link Directly 自动跳转 当前网页非官方页面/已停止访问该网页
 // @namespace    https://github.com/willbchang/userscript-qq-open-link-directly/
-// @version      1.0.0
-// @description  点击 QQ 客户端中的链接后，自动跳转中转提示页 "当前网页非官方页面" 至原网址
+// @version      1.1.0
+// @description  点击 QQ 客户端中的链接后，自动跳转中转提示页 "当前网页非官方页面" 或 "已停止访问该网页" 至原网址
 // @author       Will B Chang
-// @match        *://c.pc.qq.com/middlem.html*
+// @match        *://c.pc.qq.com/*
 // @grant        none
+// @license      AGPL-V3
 // ==/UserScript==
 
 
 (function() {
     'use strict';
     const url = new URL(window.location.href)
-    const target = url.searchParams.get('pfurl')
+    // pfurl 对应 当前网页非官方页面
+    // url 对应 已停止访问该网页
+    const target = url.searchParams.get('pfurl') || url.searchParams.get('url')
     window.location.href = target;
 })();
