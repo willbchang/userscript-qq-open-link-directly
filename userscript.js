bdf6a06c865561d0bc82bc6ba17795ec69b55a4a
Will B Chang <14329786+willbchang@users.noreply.github.com>
2023-06-28 11:17:16 +0800

    fix: url in parameter ends with double /

--- userscript.js
+++ userscript.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         QQ Open Link Directly 自动跳转 当前网页非官方页面/已停止访问该网页
 // @namespace    https://github.com/willbchang/userscript-qq-open-link-directly/
-// @version      1.1.0
+// @version      1.1.1
 // @description  点击 QQ 客户端中的链接后，自动跳转中转提示页 "当前网页非官方页面" 或 "已停止访问该网页" 至原网址
 // @author       Will B Chang
 // @match        *://c.pc.qq.com/*
@@ -15,6 +15,7 @@
     const url = new URL(window.location.href)
     // pfurl 对应 当前网页非官方页面
     // url 对应 已停止访问该网页
-    const target = url.searchParams.get('pfurl') || url.searchParams.get('url')
+    let target = url.searchParams.get('pfurl') || url.searchParams.get('url')
+    if (target.endsWith('//')) target = target.slice(0, -1)
     window.location.href = target;
 })();
